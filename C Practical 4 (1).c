067bfdf317b8a4f7e5e24e3b92fe9af89ce21823
pmlchamodya <135612189+pmlchamodya@users.noreply.github.com>
2023-07-29 13:58:53 +0530

    added file

---
+++ C Practical 4 (1).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    while(i<=100)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+}
+
+
+
+
+#include <stdio.h>
+int main()
+
+{
+    int i=0;
+    do
+    {
+        printf("%d",i);
+        i++;
+    }while(i<=100);
+}
+
+
+
+
+
+
